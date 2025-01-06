335fc51fc167b8a3f5f6231b2ca40bfe8c14c7b1
FlorentinFallon <119119355+FlorentinFallon@users.noreply.github.com>
2025-01-06 10:26:02 +0100

    initialized project and create many page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import { BrowserRouter, Routes, Route} from "react-router-dom";
 import Home from './Home';
+import Login from "./Login";
+import Register from "./Register";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </BrowserRouter>
   );
--- src/Home.js
+++ src/Home.js
@@ -1,8 +1,16 @@
+import { Box, Button, Typography } from '@mui/material'
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Home() {
   return (
-    <div>Bienvenue sur ta mere !</div>
+    <Box>
+      <Typography variant='h4' sx={{textAlign: 'center'}}>Hello</Typography>
+      <Box sx={{display: 'flex', justifyContent: 'center', gap: '10px', marginTop: '20px'}}>
+        <Button variant='contained' color='primary'><Link to="/login" style={{ textDecoration: "none", color: "inherit" }}>Click Me</Link></Button>
+        <Button variant='contained' color='secondary'><Link to="/register" style={{ textDecoration: "none", color: "inherit" }}>Click Me</Link></Button>
+      </Box>
+    </Box>
   )
 }
 
