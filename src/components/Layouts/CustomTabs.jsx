05579659dd2a28ab5e1e26af64e04fbddf3ca5c7
FlorentinFallon <119119355+FlorentinFallon@users.noreply.github.com>
2025-01-07 11:17:55 +0100

    update page customTabs

--- src/components/Layouts/CustomTabs.jsx
+++ src/components/Layouts/CustomTabs.jsx
@@ -10,35 +10,49 @@ import ArrowUp from "../../Assets/Arrow-up.svg";
 import ArrowRotate from "../../Assets/Arrow-Rotate.svg";
 import User from "../../Assets/User.svg";
 import Power from "../../Assets/Power.svg";
-<<<<<<< HEAD
 import { Link } from "react-router-dom";
 import AccountAmountCard from "../Cards/AccountAmountCard";
 import AccountInformationsCard from "../Cards/AccountInformationsCard";
 import AccountCreditLimiteCard from "../Cards/AccountCreditLimiteCard";
 import AccountAdvisorCard from "../Cards/AccountAdvisorCard";
 import AccountTransactionCard from "../Cards/AccountTransactionCard";
-=======
-import {Link} from "react-router-dom";
->>>>>>> 5f16d4380abc7b8016e8916c9ece4301d3b0a8d3
 
 function CustomTabs() {
-    const [value, setValue] = React.useState("0");
+  const [value, setValue] = React.useState("0");
 
-    const handleChange = (event, newValue) => {
-        setValue(newValue);
-    };
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
 
-    return (
-        <Box
-            sx={{
-                flexGrow: 1,
-                display: "flex",
-                height: 450,
-                mt: 5,
-            }}
+  return (
+    <Box
+      sx={{
+        flexGrow: 1,
+        display: "flex",
+        height: 450,
+        mt: 5,
+      }}
+    >
+      <TabContext value={value}>
+        <Tabs
+          orientation="vertical"
+          value={value}
+          onChange={handleChange}
+          aria-label="Vertical tabs example"
+          sx={{
+            "& .MuiTab-root": {
+              marginBottom: 1,
+              "&:hover": {
+                backgroundColor: "transparent",
+              },
+            },
+            "& .MuiTabs-indicator": {
+              backgroundColor: "#fff",
+            },
+          }}
         >
-<<<<<<< HEAD
           <Tab
+            disableRipple
             icon={
               <img
                 src={HomeIcon}
@@ -60,6 +74,7 @@ function CustomTabs() {
             }}
           />
           <Tab
+            disableRipple
             icon={
               <img
                 src={Card}
@@ -81,6 +96,7 @@ function CustomTabs() {
             }}
           />
           <Tab
+            disableRipple
             icon={
               <img
                 src={ArrowUp}
@@ -102,6 +118,7 @@ function CustomTabs() {
             }}
           />
           <Tab
+            disableRipple
             icon={
               <img
                 src={ArrowRotate}
@@ -146,166 +163,48 @@ function CustomTabs() {
         <TabPanel value="2">Historique de transactions</TabPanel>
         <TabPanel value="3">Cartes</TabPanel>
       </TabContext>
-=======
-            <TabContext value={value}>
-                <Tabs
-                    orientation="vertical"
-                    value={value}
-                    onChange={handleChange}
-                    aria-label="Vertical tabs example"
-                    sx={{
-                        "& .MuiTab-root": {
-                            marginBottom: 1,
-                            "&:hover": {
-                                backgroundColor: "transparent",
-                            },
-                        },
-                        "& .MuiTabs-indicator": {
-                            backgroundColor: "#fff",
-                        },
-                    }}
-                >
-                    <Tab
-                        disableRipple
-                        icon={
-                            <img
-                                src={HomeIcon}
-                                alt="Home"
-                                style={{
-                                    width: 24,
-                                    height: 24,
-                                    borderRadius: "50%",
-                                    backgroundColor: value === "0" ? "#e1eefc" : "transparent",
-                                    padding: 15,
-                                }}
-                            />
-                        }
-                        value="0"
-                        sx={{
-                            display: "flex",
-                            justifyContent: "center",
-                            alignItems: "center",
-                        }}
-                    />
-                    <Tab
-                        disableRipple
-                        icon={
-                            <img
-                                src={Card}
-                                alt="Card"
-                                style={{
-                                    width: 24,
-                                    height: 24,
-                                    borderRadius: "50%",
-                                    backgroundColor: value === "1" ? "#e1eefc" : "transparent",
-                                    padding: 15,
-                                }}
-                            />
-                        }
-                        value="1"
-                        sx={{
-                            display: "flex",
-                            justifyContent: "center",
-                            alignItems: "center",
-                        }}
-                    />
-                    <Tab
-                        disableRipple
-                        icon={
-                            <img
-                                src={ArrowUp}
-                                alt="ArrowUp"
-                                style={{
-                                    width: 24,
-                                    height: 24,
-                                    borderRadius: "50%",
-                                    backgroundColor: value === "2" ? "#e1eefc" : "transparent",
-                                    padding: 15,
-                                }}
-                            />
-                        }
-                        value="2"
-                        sx={{
-                            display: "flex",
-                            justifyContent: "center",
-                            alignItems: "center",
-                        }}
-                    />
-                    <Tab
-                        disableRipple
-                        icon={
-                            <img
-                                src={ArrowRotate}
-                                alt="ArrowRotate"
-                                style={{
-                                    width: 24,
-                                    height: 24,
-                                    borderRadius: "50%",
-                                    backgroundColor: value === "3" ? "#e1eefc" : "transparent",
-                                    padding: 15,
-                                }}
-                            />
-                        }
-                        value="3"
-                        sx={{
-                            display: "flex",
-                            justifyContent: "center",
-                            alignItems: "center",
-                            "&:hover": {
-                                backgroundColor: "#fff",
-                            },
-                        }}
-                    />
-                </Tabs>
-                <TabPanel value="0">Accueil</TabPanel>
-                <TabPanel value="1">Virements</TabPanel>
-                <TabPanel value="2">Historique de transactions</TabPanel>
-                <TabPanel value="3">Cartes</TabPanel>
-            </TabContext>
->>>>>>> 5f16d4380abc7b8016e8916c9ece4301d3b0a8d3
-
-            <Box
-                sx={{
-                    position: "fixed",
-                    bottom: 20,
-                    left: 20,
-                    display: "flex",
-                    flexDirection: "column",
-                    gap: 2,
-                }}
-            >
-                <Tab
-                    icon={
-                        <img
-                            src={User}
-                            alt="User"
-                            style={{
-                                width: 24,
-                                height: 24,
-                            }}
-                        />
-                    }
-                    sx={{
-                        display: "flex",
-                        justifyContent: "center",
-                        alignItems: "center",
-                    }}
-                />
-                <Link to="/login">
-                    <Tab
-                        icon={
-                            <img src={Power} alt="Power" style={{width: 24, height: 24}}/>
-                        }
-                        sx={{
-                            display: "flex",
-                            justifyContent: "center",
-                            alignItems: "center",
-                        }}
-                    />
-                </Link>
-            </Box>
-        </Box>
-    );
+      <Box
+        sx={{
+          position: "fixed",
+          bottom: 20,
+          left: 20,
+          display: "flex",
+          flexDirection: "column",
+          gap: 2,
+        }}
+      >
+        <Tab
+          icon={
+            <img
+              src={User}
+              alt="User"
+              style={{
+                width: 24,
+                height: 24,
+              }}
+            />
+          }
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        />
+        <Link to="/login">
+          <Tab
+            icon={
+              <img src={Power} alt="Power" style={{ width: 24, height: 24 }} />
+            }
+            sx={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          />
+        </Link>
+      </Box>
+    </Box>
+  );
 }
 
 export default CustomTabs;
