7c2526028da37dfe58422ae6eb405e59d54d062c
Cacahuète Dev <24205659+CacahueteSansSel@users.noreply.github.com>
2025-01-25 21:42:12 +0100

    added card creation page & more tweaks

---
+++ src/components/Layouts/CardCreationLayout.jsx
@@ -0,0 +1,103 @@
+import React, {useEffect, useState} from "react";
+import {Alert, Box, Fab, MenuItem, Select, Snackbar, TextField, Typography} from "@mui/material";
+import AddIcon from '@mui/icons-material/Add';
+import PreviewCard from "../../Assets/PreviewCard.svg";
+import {createAccount, createAccountCard, getAccountFirstCard, getCurrentAccount} from "../../Api/api";
+import Button from "@mui/material/Button";
+
+function CardCreationLayout({account, setValue}) {
+    const [snackbarOpen, setSnackbarOpen] = useState(false);
+    const [alertMessage, setAlertMessage] = useState("");
+    const [alertSeverity, setAlertSeverity] = useState("success");
+    const [cardName, setCardName] = useState("");
+    const [cardNetwork, setCardNetwork] = useState("mastercard");
+    const [cardType, setCardType] = useState("debit");
+    const [cardStyle, setCardStyle] = useState("black");
+
+    const handleCloseSnackbar = () => {
+        setSnackbarOpen(false);
+    };
+
+    const handleCardCreation = async () => {
+        if (!cardName) {
+            return;
+        }
+
+        let toSend = { 'name': cardName, 'network': cardNetwork, 'type': cardType, 'style': cardStyle };
+        try {
+            await createAccountCard(account.bban, toSend);
+        } catch (error) {
+            console.error("Erreur lors de la création :", error);
+
+            setAlertMessage("Erreur lors de la création de la carte !");
+            setAlertSeverity("error");
+        } finally {
+            setValue(0);
+        }
+    };
+
+    return (
+        <Box>
+            <Box sx={{ mb: 2, display: "flex", flexDirection: "row", gap: 2, alignItems: "center" }}>
+                <Typography variant="h4" sx={{fontWeight: "bold"}}>
+                    Créer une carte
+                </Typography>
+            </Box>
+            <Box>
+                <Box sx={{display: "flex", flexDirection: "column", justifyContent: "center", gap: 2}}>
+                    <img src={PreviewCard} alt="Card"/>
+                    <TextField
+                        label="Nom de la carte"
+                        variant="outlined"
+                        value={cardName}
+                        onChange={(e) => setCardName(e.target.value)}
+                    />
+                    <Select
+                        value={cardNetwork}
+                        variant="outlined"
+                        label="Network"
+                        onChange={(e) => setCardNetwork(e.target.value)}
+                    >
+                        <MenuItem value={'mastercard'}>Mastercard</MenuItem>
+                        <MenuItem value={'visa'}>Visa</MenuItem>
+                    </Select>
+                    <Select
+                        value={cardType}
+                        variant="outlined"
+                        label="Type"
+                        onChange={(e) => setCardType(e.target.value)}
+                    >
+                        <MenuItem value={'debit'}>Carte de débit (retrait instantané)</MenuItem>
+                        <MenuItem value={'credit'}>Carte de crédit (retrait différé)</MenuItem>
+                    </Select>
+                    <Select
+                        value={cardStyle}
+                        variant="outlined"
+                        label="Style"
+                        onChange={(e) => setCardStyle(e.target.value)}
+                    >
+                        <MenuItem value={'black'}>Edition spéciale Club du Crousti-Crabe</MenuItem>
+                    </Select>
+                    <Button variant="contained" onClick={handleCardCreation}>
+                        Créer la carte
+                    </Button>
+                </Box>
+            </Box>
+            <Snackbar
+                open={snackbarOpen}
+                autoHideDuration={6000}
+                onClose={handleCloseSnackbar}
+            >
+                <Alert
+                    onClose={handleCloseSnackbar}
+                    severity={alertSeverity}
+                    sx={{ width: "100%" }}
+                >
+                    {alertMessage}
+                </Alert>
+            </Snackbar>
+        </Box>
+    );
+}
+
+export default CardCreationLayout;
--- src/components/Layouts/CreditCard.jsx
+++ src/components/Layouts/CreditCard.jsx
@@ -1,18 +1,70 @@
-import React from "react";
-import { Box, Typography } from "@mui/material";
-import AccountCreditCardCard from "../Cards/AccountCreditCardCard";
+import React, {useEffect, useState} from "react";
+import {Box, Fab, Typography} from "@mui/material";
+import AddIcon from '@mui/icons-material/Add';
+import PreviewCard from "../../Assets/PreviewCard.svg";
+import {getAccountFirstCard} from "../../Api/api";
 
-function CreditCard() {
-  return (
-    <Box>
-      <Typography variant="h4" sx={{ fontWeight: "bold", mb: 2 }}>
-        Cartes
-      </Typography>
-      <Box>
-        <AccountCreditCardCard />
-      </Box>
-    </Box>
-  );
+function CreditCard({account, setValue}) {
+    const [card, setCard] = useState();
+    const [numbers, setNumbers] = useState(9999);
+
+    useEffect(() => {
+        async function fetchAsync() {
+            let firstCard = await getAccountFirstCard(account.bban);
+            if (firstCard === undefined) {
+                setCard(null);
+                return;
+            }
+            let lastNumbers = firstCard.numbers.split(" ")[3];
+
+            setCard(firstCard);
+            setNumbers(lastNumbers);
+        }
+
+        if (account !== undefined) fetchAsync();
+    }, [account]);
+
+    return (
+        <Box>
+            <Box sx={{ mb: 2, display: "flex", flexDirection: "row", gap: 2, alignItems: "center" }}>
+                <Typography variant="h4" sx={{fontWeight: "bold"}}>
+                    Votre carte
+                </Typography>
+                <Fab color="primary" aria-label="add" onClick={() => setValue("5")}>
+                    <AddIcon />
+                </Fab>
+            </Box>
+            {card === null || card === undefined ? (
+                <Box>
+                    <Box sx={{display: "flex", justifyContent: "center", gap: 2}}>
+                        <Box
+                            sx={{
+                                border: 1,
+                                borderColor: "#D4D4D4",
+                                borderRadius: 2,
+                                p: 2,
+                                mt: 2,
+                                width: 360,
+                                height: 180,
+                                boxSizing: "border-box",
+                            }}
+                        >
+                            <Typography variant="h6" sx={{ mb: 6, fontWeight: "bold" }}>
+                                Aucune carte associée
+                            </Typography>
+                        </Box>
+                    </Box>
+                </Box>
+            ) : (
+                <Box>
+                    <Box sx={{display: "flex", justifyContent: "center", gap: 2}}>
+                        <img src={PreviewCard} alt="Card"/>
+                        <Typography variant="h6">{card.name}</Typography>
+                    </Box>
+                </Box>
+            )}
+        </Box>
+    );
 }
 
 export default CreditCard;
