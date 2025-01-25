cf8cbe288eebf7e55efff52f27903499bfcafad7
Cacahuète Dev <24205659+CacahueteSansSel@users.noreply.github.com>
2025-01-25 20:04:11 +0100

    made transactions work, fixed some issues

--- src/components/Layouts/FormTransaction.jsx
+++ src/components/Layouts/FormTransaction.jsx
@@ -5,25 +5,30 @@ import {
   TextField,
   Button,
   Snackbar,
-  Alert,
+  Alert, Select, MenuItem,
 } from "@mui/material";
-import { createTransaction } from "../../Api/api";
+import {createTransaction, getCurrentAccount} from "../../Api/api";
 
 function FormTransaction({ onTransaction }) {
   const [iban, setIban] = useState("");
   const [amount, setAmount] = useState("");
+  const [label, setLabel] = useState("");
+  const [currency, setCurrency] = useState("USD");
   const [snackbarOpen, setSnackbarOpen] = useState(false);
   const [alertMessage, setAlertMessage] = useState("");
   const [alertSeverity, setAlertSeverity] = useState("success");
 
   const handleTransaction = async () => {
-    if (iban && amount) {
-      console.log("Données envoyées :", { iban, amount });
+    if (iban && amount && label) {
+      let toSend = { currency, 'fromIban': (await getCurrentAccount()).iban, 'toIban': iban, amount, label }
+
+      console.log("Données envoyées :", toSend);
       try {
-        await createTransaction({ iban, amount });
+        await createTransaction(toSend);
         setAlertMessage("Transaction réussie !");
         setAlertSeverity("success");
-        onTransaction({ iban, amount });
+        setSnackbarOpen(true);
+        if (onTransaction) onTransaction({ iban, amount });
       } catch (error) {
         console.error("Erreur lors de la transaction :", error);
         setAlertMessage("Erreur lors de la transaction !");
@@ -58,11 +63,27 @@ function FormTransaction({ onTransaction }) {
         }}
       >
         <TextField
+            label="Intitulé"
+            variant="outlined"
+            value={label}
+            onChange={(e) => setLabel(e.target.value)}
+        />
+        <TextField
           label="IBAN du destinataire"
           variant="outlined"
           value={iban}
           onChange={(e) => setIban(e.target.value)}
         />
+        <Select
+            value={currency}
+            variant="outlined"
+            label="Currency"
+            onChange={(e) => setCurrency(e.target.value)}
+        >
+          <MenuItem value={'USD'}>United States Dollars (USD)</MenuItem>
+          <MenuItem value={'EUR'}>Euros (EUR)</MenuItem>
+          <MenuItem value={'ROBUX'}>Robux (ROBUX)</MenuItem>
+        </Select>
         <TextField
           label="Montant"
           variant="outlined"
