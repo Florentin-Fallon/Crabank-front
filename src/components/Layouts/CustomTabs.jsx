cab4fc4e1ae4e2182d6df4f8a8f1f3e1397f4a24
FlorentinFallon <119119355+FlorentinFallon@users.noreply.github.com>
2025-01-25 22:56:28 +0100

    update et finish this website

--- src/components/Layouts/CustomTabs.jsx
+++ src/components/Layouts/CustomTabs.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import Box from "@mui/material/Box";
 import Tab from "@mui/material/Tab";
 import TabPanel from "@mui/lab/TabPanel";
@@ -10,13 +10,13 @@ import ArrowUp from "../../Assets/Arrow-up.svg";
 import ArrowRotate from "../../Assets/Arrow-Rotate.svg";
 import User from "../../Assets/User.svg";
 import Power from "../../Assets/Power.svg";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import AccountAmountCard from "../Cards/AccountAmountCard";
 import AccountInformationsCard from "../Cards/AccountInformationsCard";
 import AccountCreditLimitCard from "../Cards/AccountCreditLimitCard";
 import AccountAdvisorCard from "../Cards/AccountAdvisorCard";
 import AccountTransactionCard from "../Cards/AccountTransactionCard";
-import {getCurrentAccount, getAccountCards} from "../../Api/api";
+import { getCurrentAccount, getAccountCards } from "../../Api/api";
 import AccountCreditCardCard from "../Cards/AccountCreditCardCard";
 import Information from "../Information/Information";
 import FormTransaction from "./FormTransaction";
@@ -25,251 +25,68 @@ import CreditCard from "./CreditCard";
 import CardCreationLayout from "./CardCreationLayout";
 
 function CustomTabs() {
-    const [value, setValue] = React.useState("0");
-    const [account, setAccount] = useState();
-    const [cards, setCards] = useState([]);
+  const [value, setValue] = React.useState("0");
+  const [account, setAccount] = useState();
+  const [cards, setCards] = useState([]);
 
-    const handleChange = (event, newValue) => {
-        setValue(newValue);
-    };
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
 
-    useEffect(() => {
-        async function fetchAccountAsync() {
-            let account = await getCurrentAccount();
-            setAccount(account);
+  useEffect(() => {
+    async function fetchAccountAsync() {
+      let account = await getCurrentAccount();
+      setAccount(account);
 
-            if (account) {
-                const userCards = await getAccountCards(account.bban);
-                setCards(userCards);
-            }
-        }
+      if (account) {
+        const userCards = await getAccountCards(account.bban);
+        setCards(userCards);
+      }
+    }
 
-        fetchAccountAsync();
-    }, []);
+    fetchAccountAsync();
+  }, []);
 
-    return (
-        <Box
-            sx={{
-                flexGrow: 1,
-                display: "flex",
-                height: 450,
-                mt: 1,
-            }}
+  return (
+    <Box
+      sx={{
+        flexGrow: 1,
+        display: "flex",
+        height: 450,
+        mt: 1,
+      }}
+    >
+      <TabContext value={value}>
+        <Tabs
+          orientation="vertical"
+          value={value}
+          onChange={handleChange}
+          aria-label="Vertical tabs example"
+          sx={{
+            "& .MuiTab-root": {
+              marginBottom: 1,
+              "&:hover": {
+                backgroundColor: "transparent",
+              },
+            },
+            "& .MuiTabs-indicator": {
+              backgroundColor: "#fff",
+            },
+          }}
         >
-            <TabContext value={value}>
-                <Tabs
-                    orientation="vertical"
-                    value={value}
-                    onChange={handleChange}
-                    aria-label="Vertical tabs example"
-                    sx={{
-                        "& .MuiTab-root": {
-                            marginBottom: 1,
-                            "&:hover": {
-                                backgroundColor: "transparent",
-                            },
-                        },
-                        "& .MuiTabs-indicator": {
-                            backgroundColor: "#fff",
-                        },
-                    }}
-                >
-                    <Tab
-                        disableRipple
-                        icon={
-                            <img
-                                src={HomeIcon}
-                                alt="Home"
-                                style={{
-                                    width: 24,
-                                    height: 24,
-                                    borderRadius: "50%",
-                                    backgroundColor: value === "0" ? "#e1eefc" : "transparent",
-                                    padding: 15,
-                                }}
-                            />
-                        }
-                        value="0"
-                        sx={{
-                            display: "flex",
-                            justifyContent: "center",
-                            alignItems: "center",
-                        }}
-                    />
-                    <Tab
-                        disableRipple
-                        icon={
-                            <img
-                                src={ArrowUp}
-                                alt="ArrowUp"
-                                style={{
-                                    width: 24,
-                                    height: 24,
-                                    borderRadius: "50%",
-                                    backgroundColor: value === "1" ? "#e1eefc" : "transparent",
-                                    padding: 15,
-                                }}
-                            />
-                        }
-                        value="1"
-                        sx={{
-                            display: "flex",
-                            justifyContent: "center",
-                            alignItems: "center",
-                        }}
-                    />
-                    <Tab
-                        disableRipple
-                        icon={
-                            <img
-                                src={ArrowRotate}
-                                alt="ArrowRotate"
-                                style={{
-                                    width: 24,
-                                    height: 24,
-                                    borderRadius: "50%",
-                                    backgroundColor: value === "2" ? "#e1eefc" : "transparent",
-                                    padding: 15,
-                                }}
-                            />
-                        }
-                        value="2"
-                        sx={{
-                            display: "flex",
-                            justifyContent: "center",
-                            alignItems: "center",
-                        }}
-                    />
-                    <Tab
-                        disableRipple
-                        icon={
-                            <img
-                                src={Card}
-                                alt="Card"
-                                style={{
-                                    width: 24,
-                                    height: 24,
-                                    borderRadius: "50%",
-                                    backgroundColor: value === "3" ? "#e1eefc" : "transparent",
-                                    padding: 15,
-                                }}
-                            />
-                        }
-                        value="3"
-                        sx={{
-                            display: "flex",
-                            justifyContent: "center",
-                            alignItems: "center",
-                            "&:hover": {
-                                backgroundColor: "#fff",
-                            },
-                        }}
-                    />
-                    <Tab
-                        disableRipple
-                        icon={
-                            <img
-                                src={User}
-                                alt="User"
-                                style={{
-                                    width: 24,
-                                    height: 24,
-                                    borderRadius: "50%",
-                                    backgroundColor: value === "4" ? "#e1eefc" : "transparent",
-                                    padding: 15,
-                                }}
-                            />
-                        }
-                        value="4"
-                        sx={{
-                            display: "flex",
-                            justifyContent: "center",
-                            alignItems: "center",
-                            "&:hover": {
-                                backgroundColor: "#fff",
-                            },
-                        }}
-                    />
-                </Tabs>
-                <TabPanel
-                    value="0"
-                    sx={{
-                        margin: 0,
-                        padding: 0,
-                        ml: 2,
-                    }}
-                >
-                    <Box
-                        sx={{
-                            display: "grid",
-                            gridTemplateColumns: "repeat(3, 1fr)",
-                            gap: 2,
-                            justifyContent: "center",
-                        }}
-                    >
-                        <AccountAmountCard account={account}/>
-                        <AccountInformationsCard account={account}/>
-                        <AccountCreditLimitCard account={account}/>
-                        <AccountAdvisorCard account={account} cards={cards}/>
-                        <Link
-                            style={{textDecoration: "none", color: "inherit"}}
-                            to="#"
-                            onClick={() => setValue("2")}
-                        >
-                            <AccountTransactionCard account={account}/>
-                        </Link>
-                        <AccountCreditCardCard account={account}/>
-                    </Box>
-                </TabPanel>
-                <TabPanel value="1">
-                    <FormTransaction/>
-                </TabPanel>
-                <TabPanel value="2">
-                    <HistoryTransactions account={account}/>
-                </TabPanel>
-                <TabPanel value="3">
-                    <CreditCard account={account} setValue={setValue} />
-                </TabPanel>
-                <TabPanel
-                    sx={{
-                        width: "100%",
-                    }}
-                    value="4"
-                >
-                    <Information account={account}/>
-                </TabPanel>
-                <TabPanel value="5">
-                    <CardCreationLayout account={account} setValue={setValue} />
-                </TabPanel>
-            </TabContext>
-            <Box
-                sx={{
-                    position: "fixed",
-                    bottom: 20,
-                    left: 8,
-                    display: "flex",
-                    flexDirection: "column",
-                    gap: 2,
+          <Tab
+            disableRipple
+            icon={
+              <img
+                src={HomeIcon}
+                alt="Home"
+                style={{
+                  width: 24,
+                  height: 24,
+                  borderRadius: "50%",
+                  backgroundColor: value === "0" ? "#e1eefc" : "transparent",
+                  padding: 15,
                 }}
-<<<<<<< HEAD
-            >
-                <Link to="/login">
-                    <Tab
-                        icon={
-                            <img src={Power} alt="Power" style={{width: 24, height: 24}}/>
-                        }
-                        sx={{
-                            display: "flex",
-                            justifyContent: "center",
-                            alignItems: "center",
-                            opacity: 1,
-                        }}
-                    />
-                </Link>
-            </Box>
-        </Box>
-    );
-=======
               />
             }
             value="0"
@@ -421,6 +238,9 @@ function CustomTabs() {
         >
           <Information account={account} />
         </TabPanel>
+        <TabPanel value="5">
+          <CardCreationLayout account={account} setValue={setValue} />
+        </TabPanel>
       </TabContext>
       <Box
         sx={{
@@ -448,7 +268,6 @@ function CustomTabs() {
       </Box>
     </Box>
   );
->>>>>>> 009d206e (update 25/01/25)
 }
 
 export default CustomTabs;
